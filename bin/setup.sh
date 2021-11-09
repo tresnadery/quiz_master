ff8086ade3caebb1bb5e6a0024d31b4545afe4fb
Dery Tresna Pratama <tresnadery@gmail.com>
2021-11-09 08:47:20 +0700

    build:create setup script

---
+++ bin/setup.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#load env
+if [ -f ../.env ]
+then
+  export $(cat ../.env | sed 's/#.*//g' | xargs)
+fi
+
+cd ..
+# run gomod
+go mod init
+
+#run go mod tidy
+go mod tidy
+
+# run repository test
+cd repository
+go test -v
+
+# run usecase test
+cd ..
+cd usecase
+go test -v
+
+# run command test
+cd ..
+cd cmd
+go test -v
+
+# run create database and run migration
+export MYSQL_PWD=$DB_PASS;
+mysql -u $DB_USER \
+-e "CREATE DATABASE IF NOT EXISTS $DB_NAME;";
+
+# migrate table question
+mysql -u $DB_USER $DB_NAME < ../database/migration.sql
+
+# build app
+cd ..
+go build -o bin/quiz_master
